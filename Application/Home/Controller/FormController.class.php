6021f7c913412594d3c501822fca367e36cc0823
Honlan <493722771@qq.com>
2015-08-20 17:13:39 +0800

    add other field

--- Application/Home/Controller/FormController.class.php
+++ Application/Home/Controller/FormController.class.php
@@ -8,7 +8,7 @@ class FormController extends Controller {
 
     public function handle(){
     	if (IS_POST) {
-    		$data = array('name'=>I('name'),'mobile'=>I('mobile'),'email'=>I('email'),'company'=>I('company'),'position'=>I('position'),'timestamp'=>time());
+    		$data = array('name'=>I('name'),'mobile'=>I('mobile'),'email'=>I('email'),'company'=>I('company'),'position'=>I('position'), 'other'=>I('other'), 'timestamp'=>time());
     		M('application')->data($data)->add();
             echo json_encode(array("ok" => "1"));
     	} else {
